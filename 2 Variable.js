51f71649aaf7477b489d38b78e3c0641e8b5d659
EyimofeP <eyimofe36@gmail.com>
2024-12-23 21:32:30 +0100

    Learnt about variables

---
+++ 2 Variable.js
@@ -0,0 +1,21 @@
+// Declaring Variable with Let
+let age = 25
+console.log(age)
+
+age = 67
+console.log(age)
+
+// Declaring Variable with Var (Not Used anymore/deprecated)
+var name = "Todd"
+console.log(name)
+
+name = "Ashley"
+console.log(name)
+
+// Declaring Variable with Const (Best for Functions)
+const hasGraduated = true
+console.log(hasGraduated)
+
+// Reassigning causes an error
+// hasGraduated = false
+// console.log(hasGraduated)
