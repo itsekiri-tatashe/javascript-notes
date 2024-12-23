baf16105285580c23c45ae91330f9c0c867c5ff8
EyimofeP <eyimofe36@gmail.com>
2024-12-23 21:39:28 +0100

    Learnt about data types

---
+++ 3 Basic Data Types.js
@@ -0,0 +1,35 @@
+// Primitive D-Types
+
+// Strings
+const name = "Eyimofe"
+const message = 'How are you?'
+
+// Numbers - Int and Float
+const total = 23
+const PI = 3.14
+
+// Boolean
+const isOld = true
+const hasACar = false
+
+// Undefined
+let result
+console.log(result)
+
+result = undefined
+
+// Null
+let data = null
+
+
+// Non - Prmitive
+// Objects
+
+const person = {
+    "firstName" : "Eyimofe",
+    "lastName" : "Pinnick",
+    "age" : 22,
+    "hobbies" : ["reading", "coding", "exercising"]
+}
+
+console.log(person["hobbies"])
