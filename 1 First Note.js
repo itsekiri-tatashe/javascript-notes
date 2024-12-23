8f3878818624d4a28c2f1225a281f32f12463b6f
EyimofeP <eyimofe36@gmail.com>
2024-12-23 21:27:09 +0100

    Added first note

---
+++ 1 First Note.js
@@ -0,0 +1,9 @@
+//  Running on the terminal
+console.log("I believe in myself")
+
+// Trying to remember how to write a function
+const print = function name(anything="Hey") {
+    console.log(anything)
+}
+
+print(23 * 12)
